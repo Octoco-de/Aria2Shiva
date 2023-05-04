4c73f587510dfe4adbeec1761b8b1ece521ecad5
GoNXaS <development@gonxas.com>
2023-05-04 23:08:12 +0100

    Added download directory to config

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
+import os
+
 async def constrain_text_to_length(text, length):
     result = text
     if len(result) > length:
         result = f"{result[:length]}..."
     return result
+
+
+def validateDirectory(directory):
+    print("directory", directory)
+    # Check if the directory exists, and create it if it doesn't
+    expanded_directory = os.path.abspath(os.path.expanduser(directory))
+    os.makedirs(expanded_directory, exist_ok=True)
