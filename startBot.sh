f1366a66b2787f9df328268d9909ec1194b7ddc6
GoNXaS <development@gonxas.com>
2023-05-05 00:05:40 +0100

    Added nohup to start script

--- startBot.sh
+++ startBot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if command -v python3 &>/dev/null; then
-    python3 bot_main.py
+    nohup python3 bot_main.py &
 elif command -v python &>/dev/null; then
-    python bot_main.py
+    nohup python bot_main.py &
 else
     echo "Python not found"
     exit 1
